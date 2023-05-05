9c202d9345cd2f1de4dc9b96e42eeec308fd5752
DayoungChoii <young12430@gmail.com>
2023-05-05 22:27:09 +0900

    Chore: 프로젝트 생성

---
+++ src/test/kotlin/com/toypjt/findoutstudy/FindoutstudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toypjt.findoutstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FindoutstudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
